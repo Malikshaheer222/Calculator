1edeab5b467f5444a58b3ee2326f495cb66eb940
Shaheer Malik <139039230+Malikshaheer222@users.noreply.github.com>
2023-08-01 10:28:32 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+"use strict";
+function sum(x, y) {
+    return x + y;
+}
+function subtraction(x, y) {
+    return x - y;
+}
+function multiplication(x, y) {
+    return x * y;
+}
+function division(x, y) {
+    return x / y;
+}
+// Calculations Start here!
+let result = multiplication(245, 321);
+console.log(result);
---
+++ index.ts
@@ -0,0 +1,18 @@
+function sum(x:number, y:number)
+{
+    return x+y;
+}
+function subtraction(x:number, y:number)
+{
+      return x-y;
+}
+function multiplication(x:number, y:number){
+     return x * y;
+}
+function division(x:number, y:number){
+
+    return x /y;
+}
+            // Calculations Start here!
+let result = multiplication(245,321)
+console.log(result)
